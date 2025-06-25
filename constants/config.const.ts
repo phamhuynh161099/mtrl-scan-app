e69c9af7fc672a6f42352d35b8c4ca51133365c1
phamhuynh161099 <phamhuynh161099@gmail.com>
2025-06-25 16:26:31 +0700

    -

--- constants/config.const.ts
+++ constants/config.const.ts
@@ -1,8 +1,8 @@
 export const envConfig = {
     // This localhost is used for local run
     // NEXT_PUBLIC_API_ENDPOINT: 'http://localhost:8080/',
-    // NEXT_PUBLIC_API_ENDPOINT: 'https://wf3.hsvina.com/api/',
-    NEXT_PUBLIC_API_ENDPOINT: 'http://10.0.2.2:8080/'
+    NEXT_PUBLIC_API_ENDPOINT: 'https://mlb.hsvina.com/api/',
+    // NEXT_PUBLIC_API_ENDPOINT: 'http://10.0.2.2:8080/'
     // NEXT_PUBLIC_URL: 'http://localhost:3000/',
 
     // This "/api/" is used for deploy to PROD server
